79c5e6b5f678a343a5456d6879f84469447ce72f
jungwoolee98 <jungwoolee98@gmail.com>
2017-07-20 11:10:58 -0500

    meep

--- flaskstuff.py
+++ flaskstuff.py
@@ -6,18 +6,18 @@ import os
 import psycopg2
 import urlparse
 
-urlparse.uses_netloc.append("postgres")
+#urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
 print 'hello', url
 
-conn = psycopg2.connect(
+"""conn = psycopg2.connect(
     database=url.path[1:],
     user=url.username,
     password=url.password,
     host=url.hostname,
     port=url.port
-)
+)"""
 
 app=Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/sample_db'
